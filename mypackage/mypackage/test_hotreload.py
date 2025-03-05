213eea9468331bb0f4494d03bba534d6bc3a4415
Pierce Freeman <piercefreeman@gmail.com>
2025-03-05 15:38:08 -0800

    Clean up logging for clearer POC

--- mypackage/mypackage/test_hotreload.py
+++ mypackage/mypackage/test_hotreload.py
@@ -28,35 +28,36 @@ def global_fn(msg: str, count: int) -> str:
     Returns:
         str: A completion message
     """
+    # The heavy dependencies should have already been imported by the main environment
+    from mypackage.app import run_everything
+    run_everything()
+
     for i in range(count):
         print(f"{i+1}: {msg}")
-        time.sleep(0.5)
     return f"Completed {count} iterations"
 
 def main():
     """Run tasks with a specific runner."""
     package_path = str(Path(".").absolute())
-    runner_name = "test_hotreload"
+    runner_name = "[test_hotreload]"
     
     start = time.time()
+    print(f"{runner_name}: This should be the main time expenditure...")
     with isolate_imports(package_path) as runner:
         print(f"{runner_name}: Imports have been loaded in an isolated process in {time.time() - start}s")
         
-        # Execute a function in the forked process
-        print(f"\n{runner_name}: Executing function in forked process...")
-        result = runner.exec(
-            global_fn,
-            f"Hello from {runner_name}!",
-            3
-        )
-        print(f"{runner_name} result: {result}")
+        for _ in range(2):
+            print("-" * 80)
+            start = time.time()
 
-        # Wait 5s
-        # Then communicate with the forked process
-        # And print the output
-        print("Waiting 2s for results...")
-        time.sleep(0.1)
-        print("Communicating with the forked process...")
-        result = runner.communicate_isolated(result)
-        print(f"{runner_name} final result: {result}")
+            # Execute a function in the forked process
+            print(f"\n{runner_name}: Executing function in forked process...")
+            result = runner.exec(global_fn, f"Hello from {runner_name}!", 3)
+            print(f"{runner_name} result: {result} in {time.time() - start}s")
+
+            # Then communicate with the forked process to get the final function result
+            # And print the output
+            print("Communicating with the forked process...")
+            result = runner.communicate_isolated(result)
+            print(f"{runner_name} final result: {result} in {time.time() - start}s")
 
